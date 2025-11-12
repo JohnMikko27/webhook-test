4230354e2908a3e0e44f7a96c7e226f322b3038e
Mikko <realmiks27@gmail.com>
2025-11-12 11:06:26 -0800

    fix bug

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.post("/notion-webhook", express.json(), async (req, res) => {
     const r = await fetch("https://discord.com/api/webhooks/1437892798308159538/80EmXtMP42U9z6OvnC0DpOLYVw7AkvQU12gjqgtcZ7peO9Q7yx6FExFfB4BeHvdYTqS7", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ content: String(data.data.properties["What’s the issue?"].title[0].plain_text).slice(0, 1900) })
+        body: JSON.stringify({ content: `New bug submitted: 
+            ${String(data.data.properties["What’s the issue?"].title[0].plain_text).slice(0, 1900)}
+            at: ${data.data.properties['Submission time'].created_time}
+            ` })
     });
     res.status(r.ok ? 200 : 502).send(await r.text());
 });
