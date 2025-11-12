70d4f2490083a7ec3899bb6aaf2e1f227696f457
Mikko <realmiks27@gmail.com>
2025-11-12 15:36:11 -0800

    fix bug

--- app.js
+++ app.js
@@ -7,23 +7,14 @@ app.get("/", (req, res) => {
 
 app.post("/notion-webhook", express.json(), async (req, res) => {
     const data = req.body || {};
-    console.log("==========req.body=============")
-    console.log(req.body)
-    console.log("==========req.body=============")
-    console.log("==========data.url=============")
-    console.log(data.data.url)
-    console.log("==========data.url=============")
-    console.log("================data.data.properties[What’s the issue?]==================")
-    console.log(data.data.properties)
-    console.log("================data.data.properties[What’s the issue?]==================")
     const r = await fetch("https://discord.com/api/webhooks/1437892798308159538/80EmXtMP42U9z6OvnC0DpOLYVw7AkvQU12gjqgtcZ7peO9Q7yx6FExFfB4BeHvdYTqS7", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ content: `NEW ISSUE SUBMITTED 
-Bug: ${String(data.data.properties["What’s the issue?"].title[0].plain_text).slice(0, 1900)}
-Submitted at: ${data.data.properties['Submission time'].created_time}
-Link: ${data.data.url}
-            ` })
+                Bug: ${String(data.data.properties["What’s the issue?"].title[0].plain_text).slice(0, 1900)}
+                Submitted at: ${data.data.properties['Submission time'].created_time}
+                Link: ${data.data.url}
+        ` })
     });
     res.status(r.ok ? 200 : 502).send(await r.text());
 });
